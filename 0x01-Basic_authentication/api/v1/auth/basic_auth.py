a147ff5c13878a76e4b61a45d15ebc6b390a0c18
Jerry Wonder <jerrycul2001@gmail.com>
2023-08-16 19:15:58 +0100

    feat: add method to extract basic info from auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -28,8 +28,23 @@ class BasicAuth(Auth):
             if base64_authorization_header and isinstance(
                 base64_authorization_header, str
             ):
-                encoded_str = base64_authorization_header.encode('utf-8')
+                encoded_str = base64_authorization_header.encode("utf-8")
                 base64str = base64.b64decode(encoded_str)
                 return base64str.decode()
         except binascii.Error:
             return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """Extracts the user email and password from the decoded auth_header string"""
+        email = None
+        password = None
+        if (
+            decoded_base64_authorization_header
+            and isinstance(decoded_base64_authorization_header, str)
+            and decoded_base64_authorization_header.find(":") != -1
+        ):
+            email, password = decoded_base64_authorization_header.split(":")
+
+        return (email, password)
