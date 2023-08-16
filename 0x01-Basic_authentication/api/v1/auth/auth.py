8cabb5343bb47c931120c9c20a7f687fa3680fdd
Jerry Wonder <jerrycul2001@gmail.com>
2023-08-16 17:43:33 +0100

    feat: implement endpoint whitelisting

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,12 @@ class Auth:
             - path (str) the path to be checked
             - excluded_paths (List) the paths that are originally excluded
         """
-        return False
+        if path and excluded_paths:
+            if path[-1] != '/':
+                path += '/'
+            if path in excluded_paths:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Checks the request object for an authorization header"""
