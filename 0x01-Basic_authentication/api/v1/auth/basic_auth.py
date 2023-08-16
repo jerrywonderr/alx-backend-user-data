49caa3bb031de5a8024baa58a63ae011310880b5
Jerry Wonder <jerrycul2001@gmail.com>
2023-08-16 18:34:49 +0100

    feat: add method to extract base64 part of auth_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic auth class"""
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Method that extracts the base64 part of the authorization header"""
+        if (
+            authorization_header
+            and isinstance(authorization_header, str)
+            and authorization_header.startswith("Basic ")
+        ):
+            return authorization_header[5:]
+        return None
