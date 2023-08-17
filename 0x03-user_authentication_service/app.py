62e1fe6114e459c9713e2742baf447900f9f504d
Jerry Wonder <jerrycul2001@gmail.com>
2023-08-18 00:38:44 +0100

    feat: add endpoint to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,12 +1,26 @@
 from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
 
+auth_inst = Auth()
 app = Flask()
 
 
-app.get('/', strict_slashes=False)
+@app.get('/', strict_slashes=False)
 def index():
     """Index route on GET"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.post('/users', strict_slashes=False)
+def users():
+    """Register a user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        auth_inst.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except ValueError:
+        jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
